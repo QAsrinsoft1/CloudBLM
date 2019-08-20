ef0e23ff31446c8b1225f44d9021d9efcf902ff8
Ajithkumar.Gopal <Ajithkumar.gopal@srinsofttech.com>
2019-08-20 17:26:36 +0530

    Change Done by Ajith

--- src/test/java/CloudBLM/GitHub/CreateProject.java
+++ src/test/java/CloudBLM/GitHub/CreateProject.java
@@ -4,17 +4,11 @@ import java.awt.AWTException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+
 
 public class CreateProject {
 	
@@ -25,15 +19,17 @@ public class CreateProject {
 	public static HSSFWorkbook wb;
 	
 	public static void ProjectValidation() throws InterruptedException, AWTException, IOException {
-		//somethiung
-	driver.findElement(By.xpath("//a[text()=' Create ']")).click();
-	Thread.sleep(2000);
+		
+		//something
+	       driver.findElement(By.xpath("//a[text()=' Create']")).click();
+	       Thread.sleep(2000);
 	
 	
 			driver.findElement(By.xpath("//*[@id='projectName']")).click();
 			Thread.sleep(4000);
 			driver.findElement(By.xpath("(//*[@class='form-group'])[1]")).click();
 			Thread.sleep(2000);
+			
 			// Validate the validation message
 			String validationmessage = "Project Name is required";
 			String text = driver.findElement(By.xpath("//*[text() ='Project Name is required']")).getText();
