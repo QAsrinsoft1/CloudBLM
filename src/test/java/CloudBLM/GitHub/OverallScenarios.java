a3a3ebc63fe0db6b0c3113b4f75dcdf7327e3a96
fazil <fazilfcut@gmail.com>
2019-08-21 16:53:38 +0530

    Update the script for Overall scenarios

--- src/test/java/CloudBLM/GitHub/OverallScenarios.java
+++ src/test/java/CloudBLM/GitHub/OverallScenarios.java
@@ -29,7 +29,9 @@ package CloudBLM.GitHub;
 			System.setProperty("webdriver.chrome.driver",
 					"D:\\Selenium files\\Workspace\\SeleniumTraining\\Driver2\\chromedriver.exe");
 			driver = new ChromeDriver();
+			
 			// Login the application URL
+			
 			driver.get("http://192.168.1.154/blm/");
 			driver.manage().window().maximize();
 			driver.findElement(By.xpath("//*[@id='login-email']")).sendKeys("qc@sst.com");
@@ -39,13 +41,15 @@ package CloudBLM.GitHub;
 			driver.findElement(By.xpath("//*[text() =' Create ']")).click();
 			System.out.println("pass");
 
-			// touch the project name field but not entered the value
-
+			// Without entered the project name
+			
 			driver.findElement(By.xpath("//*[@id='projectName']")).click();
 			Thread.sleep(4000);
 			driver.findElement(By.xpath("(//*[@class='form-group'])[1]")).click();
 			Thread.sleep(2000);
+			
 			// Validate the validation message
+			
 			String validationmessage = "Project Name is required";
 			String Validusername = driver.findElement(By.xpath("//*[text() ='Project Name is required']")).getText();
 			System.out.println(Validusername);
@@ -56,7 +60,7 @@ package CloudBLM.GitHub;
 			}
 
 			// Enter the inValid userName and validate the validation message
-
+			
 			WebElement Projectname = driver.findElement(By.xpath("//*[@id='projectName']"));
 			Projectname.sendKeys("Sasasa232323%$#^%$^%@$^sasa");
 			driver.findElement(By.xpath("(//*[@class='form-group'])[1]")).click();
@@ -72,12 +76,12 @@ package CloudBLM.GitHub;
 			Thread.sleep(4000);
 
 			// Enter the Valid userName
-
+			
 			driver.findElement(By.xpath("//*[@id='projectName']")).sendKeys("Sasasasasa");
 			Thread.sleep(2000);
 
 			// click the project type field and Select the respective project sub type
-
+			
 			driver.findElement(By.xpath("(//*[@class='form-group'])[1]")).click();
 			Thread.sleep(2000);
 			WebElement projectid = driver.findElement(By.xpath("//*[@formcontrolname='ProjectTypeID']"));
@@ -90,7 +94,7 @@ package CloudBLM.GitHub;
 			Thread.sleep(2000);
 
 			// click the project sub type field and Select the respective project sub type
-
+			
 			driver.findElement(By.xpath("(//*[@class ='form-group'])[2]")).click();
 			WebElement projectsubtype = driver.findElement(By.xpath("//*[@formcontrolname='ProjectSubTypeID']"));
 			Select sel = new Select(projectsubtype);
@@ -101,31 +105,7 @@ package CloudBLM.GitHub;
 			}
 			Thread.sleep(2000);
 
-			/*// touch the Life cycle template but not select the value
-
-			driver.findElement(By.xpath("(//*[@class ='form-group'])[3]")).click();
-			WebElement lifecycletemplate = driver.findElement(By.xpath("//*[@formcontrolname='LifecycleTemplate']"));
-			Select sel1 = new Select(lifecycletemplate);
-			sel1.selectByIndex(1);
-			Thread.sleep(5000);
-			String text4 = lifecycletemplate.getText();
-			{
-				System.out.println(text4);
-			}*/
-			/*
-			 * //get the validations messsage String gettex =
-			 * "Lifecycle Template is required "; System.err.println("pass"); WebElement
-			 * element3 =
-			 * driver.findElement(By.xpath("//div[text()='Lifecycle Template is required ']"
-			 * )); String sss = element3.getText(); System.out.println("asas"+ sss);
-			 * System.out.println("pass1"); if (gettex.equals(sss)) {
-			 * System.out.println("Both validations are same Lifecycle Template is required"
-			 * ); } else { System.out.println("Both validations are different");
-			 * 
-			 * } Thread.sleep(4000);
-			 */
-
-			// touch the Life cycle template and select the value
+			// Validate the Life cycle template and select the value
 
 			driver.findElement(By.xpath("(//*[@class ='form-group'])[3]")).click();
 			WebElement element4 = driver.findElement(By.xpath("//*[@formcontrolname='LifecycleTemplate']"));
@@ -137,7 +117,8 @@ package CloudBLM.GitHub;
 			}
 			Thread.sleep(2000);
 
-			// touch the Construction type and select the value
+			// Validate the Construction type and select the value
+			
 			driver.findElement(By.xpath("(//*[@class ='form-group'])[4]")).click();
 			WebElement Construction = driver.findElement(By.xpath("//*[@formcontrolname='ConstructionTypeID']"));
 			Select sel4 = new Select(Construction);
@@ -148,7 +129,8 @@ package CloudBLM.GitHub;
 			}
 			Thread.sleep(2000);
 
-			// touch the contract type and select the value
+			// Validate the contract type and select the value
+			
 			driver.findElement(By.xpath("(//*[@class ='form-group'])[5]")).click();
 			WebElement contract = driver.findElement(By.xpath("//*[@formcontrolname='ContractTypeID']"));
 
@@ -160,11 +142,10 @@ package CloudBLM.GitHub;
 			}
 			Thread.sleep(2000);
 
-			// touch the start date and validate the validation message
-
-			Thread.sleep(2000);
+			// Validate the start date and validate the validation message		
+			
 			driver.findElement(By.xpath("(//*[@class ='e-input e-lib e-keyboard'])[1]")).click();
-
+			Thread.sleep(2000);
 			driver.findElement(By.xpath("(//*[@class ='e-input e-lib e-keyboard'])[2]")).click();
 			String startvalid = "Start date is required";
 			String dates = driver.findElement(By.xpath("//*[text() =' Start date is required']")).getText();
@@ -175,14 +156,15 @@ package CloudBLM.GitHub;
 			}
 			Thread.sleep(2000);
 
-			// touch the start date and enter the value
+			// Validate the start date and enter the value
+			
 			driver.findElement(By.xpath("(//*[@class ='e-input e-lib e-keyboard'])[1]")).sendKeys("08-08-2019");
 			Thread.sleep(2000);
 
-			// touch the End date and not select the value
-			Thread.sleep(2000);
+			// Validate the End date and not select the value
+			
 			driver.findElement(By.xpath("(//*[@class ='e-input e-lib e-keyboard'])[2]")).click();
-
+			Thread.sleep(2000);
 			driver.findElement(By.xpath("(//*[@class ='e-input e-lib e-keyboard'])[1]")).click();
 			String Enddate = "Start date is required";
 			String end = driver.findElement(By.xpath("//*[text() =' End date is required']")).getText();
@@ -194,7 +176,7 @@ package CloudBLM.GitHub;
 			Thread.sleep(2000);
 
 			// Validate the validation message in end date
-
+			
 			WebElement validenddate = driver.findElement(By.xpath("(//*[@class ='e-input e-lib e-keyboard'])[2]"));
 			validenddate.sendKeys("08-08-2011");
 			Thread.sleep(2000);
@@ -211,16 +193,18 @@ package CloudBLM.GitHub;
 			Thread.sleep(2000);
 			validenddate.clear();
 
-			// touch the End date and enter the Value
+			// Validate the End date and enter the Value
+			
 			driver.findElement(By.xpath("(//*[@class ='e-input e-lib e-keyboard'])[2]")).sendKeys("08-08-2029");
 			Thread.sleep(2000);
 
 			// Enter the valid data in project Address field
-
+			
 			driver.findElement(By.xpath("//*[@formcontrolname ='ProjectAddress']")).sendKeys("dsdsdsdsdsds");
 			Thread.sleep(2000);
 
-			// touch the description field but not enter the value
+			// Validate the description field but not enter the value
+			
 			driver.findElement(By.id("Description")).click();
 			Thread.sleep(2000);
 			driver.findElement(By.xpath("//*[@class='btn btn-primary']")).click();
@@ -234,34 +218,14 @@ package CloudBLM.GitHub;
 			driver.findElement(By.xpath("(//*[@class ='e-input e-lib e-keyboard'])[1]")).click();
 			Thread.sleep(2000);
 
-			// touch the description field and the enter the value
+			// Validate the description field and the enter the value
+			
 			driver.findElement(By.id("Description")).sendKeys("cgJHSAahgvfhgdzvcGXZSVcfdhgfdjhsaf");
-			Thread.sleep(2000);
-
-			// Upload the respective image
-			WebElement upload = driver.findElement(By.xpath("//*[@class = 'fa fa-upload fa-2x pointer large']"));
-			Thread.sleep(2000);
-			upload.click();
-			Thread.sleep(2000);
-			Screen screen = new Screen();
-			screen.type("C:\\Users\\Ajithkumar.Gopal\\Pictures\\Saved Pictures\\tulips-1531279-640x480.jpg");
-			// upload.sendKeys("C:\\Users\\Ajithkumar.Gopal\\Pictures\\Saved
-			// Pictures\\tulips-1531279-640x480.jpg");
-			Thread.sleep(5000);
-			Robot robot = new Robot();
-			robot.keyPress(KeyEvent.VK_ENTER);
-			robot.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(4000);
-
-			/*
-			 * @Test(enabled = false) public void cancel() throws Exception { // cancel the
-			 * project Thread.sleep(2000);
-			 * driver.findElement(By.xpath("(//*[@class ='btn btn-secondary'])[1]")).click()
-			 * ; }
-			 */
+			Thread.sleep(2000);		
 
 			// clear the project
-			Thread.sleep(4000);
-			driver.findElement(By.xpath("(//*[@class ='btn btn-secondary'])[2]")).click();
+	
+			driver.findElement(By.xpath("(//*[@class ='btn btn-secondary'])[2]")).click();		
+			Thread.sleep(2000);
 		}
 }
